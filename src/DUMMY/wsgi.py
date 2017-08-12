a377cb6e0700d3e53c50b2daf601900726640a4c
Dennis <denniskot@gmail.com>
2017-08-13 02:44:58 +0300

    wsgi.py adjusted

--- src/DUMMY/wsgi.py
+++ src/DUMMY/wsgi.py
@@ -8,7 +8,10 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DUMMY.settings")
